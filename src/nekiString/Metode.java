214fe5af34ec9b0500d54174a695074587cdb864
DejanMladjenovic <dejo.muscar@gmail.com>
2016-04-08 10:48:31 +0200

    Pocetak

---
+++ src/nekiString/Metode.java
@@ -0,0 +1,35 @@
+package nekiString;
+
+public class Metode {
+	
+	public int duzinaStringa(){	
+		
+		return 0;
+	}
+	
+	public char[] parniKarakteri(){
+		
+		return null;
+	}
+	
+	public char[] neparniKarakteri(){
+		
+		return null;
+	}
+	
+	public int velikaSlova(){
+		
+		return 0;
+	}
+	
+	public int malaSlova(){
+		
+		return 0;
+	}
+	
+	public char[] neSlova(){
+		
+		return null;
+	}
+
+}
